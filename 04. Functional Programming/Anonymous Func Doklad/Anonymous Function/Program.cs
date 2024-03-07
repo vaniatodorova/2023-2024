f5b6b8681a49b1c3004d5ff0a6bd24821bbcc4d8
Vania <veiniatodorova@abv.bg>
2024-03-07 22:27:26 +0200

    uploaded doklad

---
+++ 04. Functional Programming/Anonymous Func Doklad/Anonymous Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Anonymous_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> doubleNumber = delegate (int x)
+            {
+                return x * 2;
+            };
+
+            int result = doubleNumber(5); // result ще бъде 10
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ 04. Functional Programming/Anonymous Func Doklad/Functions with internal state/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Functions_with_internal_state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Func<int, int>> createAccumulator = () =>
+            {
+                int sum = 0;
+                return (int x) =>
+                {
+                    int currentValue = sum; // Capture the current value of sum
+                    currentValue += x;
+                    return currentValue;
+                };
+            };
+
+            var accumulator = createAccumulator();
+            Console.WriteLine(accumulator(5)); // Outputs 5
+            Console.WriteLine(accumulator(3)); // Outputs 3
+
+        }
+    }
+}
